990a9dbae2ab96a3817759be0c9efa9801f50817
Kwangsiklee <lks21c@gmail.com>
2018-04-18 15:18:42 +0900

    add module support

--- sync.sh
+++ sync.sh
@@ -5,12 +5,30 @@ bin=`cd "$bin"; pwd`
 export SERVERLIST="${bin}/serverlist/serverlist_$1"
 
 for server in `cat "$SERVERLIST"`; do
+  # Print server
   echo $server;
+
+  # rsync
   rsync -av metricbeat-6.1.4-linux-x86_64 root@$server:~/;
+
+  # make symbolic link
   ssh $server "ln -s /root/metricbeat-6.1.4-linux-x86_64 /root/metricbeat"
+
+  # copy metricbeat.yml forcefully
   scp -r config/metricbeat.yml root@$server:~/metricbeat;
+
+  if [ "$2" == "zookeeper" ]; then
+      scp -r config/modules.d/zookeeper.yml.disabled root@$server:~/metricbeat/modules.d/;
+  else
+  fi
+
+  # copy sbin for script execution
   scp -r sbin root@$server:~/metricbeat;
+
+  # change owner to root
   ssh $server "chown -R root /root/metricbeat-6.1.4-linux-x86_64"
+
+  # add exec permmision to scripts
   ssh $server "chmod +x /root/metricbeat/sbin/*.sh"
 done
 
---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ "$2" == "zk" ]; then
+    echo "You have access!"
+else
+    echo "ACCESS DENIED!"
+fi
